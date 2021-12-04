6eac0e6919698773578313f53c0579d85e3dd5ff
Josiah Khoo <josiahkhooshaoqi@gmail.com>
2021-12-04 17:26:40 +0800

    First commit

---
+++ main.py
@@ -0,0 +1,29 @@
+# Part 0
+"""
+Parse data 
+"""
+
+# Part 1
+def get_emission_parameters():
+    pass
+
+def get_emission_probabilities():
+    pass
+
+def simple_sentiment_analysis():
+    pass
+
+# Do part 2 and 3 together
+# Part 2
+def get_transition_parameters():
+    pass
+
+def viterbi():
+    pass
+
+# Part 3
+def find_best_output_sequences():
+    pass
+
+if __name__ == "__main__":
+    pass
