9fbd541ea2bbbabf786184c1daa3dbaf23b957f0
Teo-KJ <48685014+Teo-KJ@users.noreply.github.com>
2021-12-06 22:46:49 +0800

    Update emission.py

--- HMM/emission.py
+++ HMM/emission.py
@@ -1,6 +1,20 @@
 from typing import List
-
+import numpy as np
+from utils import *
 
 def get_emission_parameters(features_encoded: List[List[int]], labels_encoded: List[List[int]]) -> List[List[int]]:
+    # x - observation, y - states
+    # b(x) = count(y->x)/count(y)
     # TODO: PART 1 - Implement this
-    pass
+    
+    numObservations = max(_flatten(features_encoded))
+    numOfStates = max(_flatten(labels_encoded))
+
+    emission = np.zeros((numOfStates, numObservations))
+
+    for i, j in zip(numOfStates, numObservations):
+        for s, o in zip(i, j):
+            emission[s, o] += 1
+
+    emission_params = emission / emission.sum(axis=0)
+    return emission_params
